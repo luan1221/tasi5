5b3a3a6466855497e9cb320a7dc67d850b017ce1
luan1221 <lssa@cin.ufpe.br>
2022-07-12 11:03:24 -0300

    ativ1 task 2 increased chat

--- atv1_sockets/server.js
+++ atv1_sockets/server.js
@@ -4,25 +4,43 @@ let clients = []
 
 const handleConnection = socket => {
   console.log("We had a connection!")
-  socket.write('Welcome to chat, please type your name before your messages')
-  
-  clients.push(socket)
-  
+
+  socket.allowMessages = false
+
+  socket.write('Welcome to chat, please type your name')
+
   socket.on('end', () => {
     console.log('A client has been disconnected')
   })
 
   socket.on('data', data => {
+    
     const message = data.toString()
-    if (message === 'end') {
-      socket.end()
+
+    if (!socket.allowMessages) {
+      socket.name = message
+      clients.push(socket)
+      socket.allowMessages = true
+    } else {
+      if (message === ':q') {
+        clients = clients.filter(client => client !== socket)
+        socket.end()
+      }
+      clients.forEach(client => {
+        if (client.name === socket.name) return
+        client.write(`[${socket.name}]: ${message}`)
+      })
     }
-    clients.forEach(client => {
-      if (client === socket) return 
-      client.write(message)
-    })
+
   })
 }
+
+// const find = (socket) => {
+//   clients.forEach(client => {
+//     if (client == socket) return true
+//   })
+//   return false
+// }
 
 const server = net.createServer(handleConnection)
 
